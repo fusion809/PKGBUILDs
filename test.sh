1cf25bb864db6d4c13c0c5f5d8d72b4439d2b759
fusion809 <brentonhorne77@gmail.com>
2016-05-28 06:50:10 +1000

    Updating

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
-for i in "*/"
+for i in */
 do
 	cd "$i"
 	pkgver=$(sed -n 's/pkgver=//p' PKGBUILD)
 	pkgrel=$(sed -n 's/pkgrel=//p' PKGBUILD)
 	updpkgsums
-	makepkg -sfC --noconfirm || printf "Building $i-$pkgver-$pkgrel failed!"
+	makepkg -sf --noconfirm || printf "Building $i-$pkgver-$pkgrel failed!"
 	du "*$pkgver-$pkgrel*.pkg.tar.xz" > namcap-$pkgver-$pkgrel-pkg.log
 	namcap "*$pkgver-$pkgrel*.pkg.tar.xz" >> namcap-$pkgver-$pkgrel-pkg.log
 	namcap PKGBUILD >> namcap-$pkgver-$pkgrel-pkgbuild.log
