12bcd069b3d1ec20306e6b74b88f6fe6823870e8
fusion809 <brentonhorne77@gmail.com>
2016-05-28 05:56:38 +1000

    Adding test script

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ do
 	pkgver=$(sed -n 's/pkgver=//p' PKGBUILD)
 	pkgrel=$(sed -n 's/pkgrel=//p' PKGBUILD)
 	makepkg -sfC --noconfirm || printf "Building $i-$pkgver-$pkgrel failed!"
-	du $i-$pkgver-$pkgrel.pkg.tar.xz > namcap-pkg.log
-	namcap $i-$pkgver-$pkgrel.pkg.tar.xz >> namcap-pkg.log
+	du "$i-$pkgver-$pkgrel.pkg.tar.xz" > namcap-pkg.log
+	namcap "$i-$pkgver-$pkgrel.pkg.tar.xz" >> namcap-pkg.log
 	namcap PKGBUILD >> namcap-pkgbuild.log
 	cd -
 done
